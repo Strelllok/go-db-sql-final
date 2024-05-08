cf68586dfc1894a01a8d29a7359b8233002e3bd1
Artem <Strelocek@mail.ru>
2024-05-08 22:55:00 +0300

    попытка 6 исправил Ретюрны

--- parcel.go
+++ parcel.go
@@ -41,7 +41,7 @@ func (s ParcelStore) Get(number int) (Parcel, error) {
     row := s.db.QueryRow("SELECT number, client, status, address, created_at FROM parcel WHERE number = :number", sql.Named("number", number))
     err := row.Scan(&p.Number, &p.Client, &p.Status, &p.Address, &p.CreatedAt)
 	if err != nil {
-		return p, err
+		return Parcel{}, err
 	}
 
 	return p, nil
@@ -61,7 +61,7 @@ func (s ParcelStore) GetByClient(client int) ([]Parcel, error) {
 		p := Parcel{}
 		err := rows.Scan(&p.Number, &p.Client, &p.Status, &p.Address, &p.CreatedAt)
 		if err != nil {
-			return res, err
+			return nil, err
 		}
 		res = append(res, p)
 	}
